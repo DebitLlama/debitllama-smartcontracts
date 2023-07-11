f7edb23d50c19d7f83d6a5f15365451b0211d27d
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-07-11 06:36:50 +0200

    created deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,6 +1,16 @@
 import { ethers } from "hardhat";
 
 async function main() {
+  const verifierFactory = await ethers.getContractFactory(
+    "contracts/PaymentIntentVerifier.sol:Verifier",
+  );
+  const Verifier = await verifierFactory.deploy();
+  await Verifier.deployed();
+  const DirectDebitFactory = await ethers.getContractFactory("DirectDebit");
+  const directDebit = await DirectDebitFactory.deploy(Verifier.address);
+  await directDebit.deployed();
+
+  console.log("Direct Debit is deployed to : ", directDebit.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
